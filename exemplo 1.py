f0ae25eed8746957a436addd8d57044ea7360223
vinycius souza <vinycius293@gmail.com>
2022-02-11 15:42:57 -0300

    first commit

---
+++ exemplo 1.py
@@ -0,0 +1,8 @@
+cod = 10
+sal = 1500.00
+nome = 'Jamal'
+situacao = True
+
+tipo = type(sal)
+
+print("Código:", cod, "Nome:", nome, "O salario atual:", sal)
---
+++ exibirvariaveis.py
@@ -0,0 +1,9 @@
+cod = int(input("Digite o codigo do funcionario "))
+nome = input ("Digte o nome do funcionario ")
+salario = float(input("informe o salario "))
+ativo = True
+
+print("Codigo: %d " % cod)
+print("nome: %s " % nome)
+print("salario: %d " % salario )
+print("Ativo: %d " % ativo)
